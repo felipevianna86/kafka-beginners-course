ad40c8e6e5af380648cfda0640bb9c1b2d0c546c
felipevianna86 <felipe.vianna86@gmail.com>
2021-05-12 16:24:32 -0300

    #1 - Realizando testes com Producer através do Properties.

--- src/main/java/com/github/felipe/kafka/tutorial01/ProducerDemo.java
+++ src/main/java/com/github/felipe/kafka/tutorial01/ProducerDemo.java
@@ -1,8 +1,29 @@
 package com.github.felipe.kafka.tutorial01;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "0.0.0.0:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello!");
+
+        producer.send(record);
+
+        producer.flush();
+
+        producer.close();
     }
 }
